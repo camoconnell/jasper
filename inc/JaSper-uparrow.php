0b34621a326565fe857efe56e7806bfb7f3a3028
cam o'connell <camoconnell@gmail.com>
2014-12-13 15:14:01 +1300

    fix up arrow svg

--- inc/JaSper-uparrow.php
+++ inc/JaSper-uparrow.php
@@ -1,19 +1,7 @@
 <div class="arrow-up">
 	<div class="inner">
-	    <svg
-	        xmlns="http://www.w3.org/2000/svg"
-	        version="1.1"
-	        x="0"
-	        y="0"
-	        width="22"
-	        height="19"
-	        viewBox="0"
-	        enable-background="new 0 0 22 19"
-	        xml:space="preserve">
-	            <path
-	            fill="#fefbf2"
-	            d="M21.9 19H0L10.9 0 21.9 19zM3.2 17.1h15.4L10.9 3.7 3.2 17.1z"
-	            />
+	    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 22 19" xml:space="preserve">
+	        <path fill="#fefbf2" d="M21.9 19H0L10.9 0 21.9 19zM3.2 17.1h15.4L10.9 3.7 3.2 17.1z" />
 	    </svg>
 	</div>
 </div>
